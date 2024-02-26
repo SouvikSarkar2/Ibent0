d711ff75b4cf7d54880dd566c95fb87ce8bd3f69
Souvik Sarkar <12345souvik12345@gmail.com>
2024-02-26 20:46:42 +0530

    Homepage Initilize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="bg-secondary h-full w-full">
-      <div className="text-text">&quot;Plan. Schedule. Connect.&quot;</div>
+    <main className="h-full w-full text-8xl font-bold bg-black flex justify-center items-center flex-col">
+      <div className="text-primary">&quot;Plan. Schedule. Connect.&quot;</div>
+      <div className="text-secondary bg-white w-full text-center">
+        &quot;Plan. Schedule. Connect.&quot;
+      </div>
+      <div className="text-tertiary">&quot;Plan. Schedule. Connect.&quot;</div>
+      <div className="text-outline bg-white w-full text-center">
+        &quot;Plan. Schedule. Connect.&quot;
+      </div>
+      <div className="text-extra">&quot;Plan. Schedule. Connect.&quot;</div>
+      <div className="text-text  bg-white w-full text-center">
+        &quot;Plan. Schedule. Connect.&quot;
+      </div>
+      <div className="absolute right-2 bottom-2"></div>
     </main>
   );
 }
